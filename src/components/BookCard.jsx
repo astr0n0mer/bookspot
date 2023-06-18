0e7e35a7dd8029157de674f3ae8a4d5d62314ab0
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-06-18 21:08:22 +0530

    Create the BookCard component

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,35 @@
+export default function BookCard({
+  book: { title, authors, cover, summary },
+  ...props
+}) {
+  return (
+    <figure
+      className="group grid cursor-pointer grid-cols-2 items-start gap-4 rounded-lg p-4 shadow-md transition hover:shadow-xl"
+      {...props}
+    >
+      <div className="overflow-hidden rounded-xl">
+        <img
+          src={cover}
+          alt={title}
+          className="w-full transition-transform group-hover:scale-105"
+        />
+      </div>
+
+      <figcaption>
+        <h3 className="text-lg font-semibold sm:text-xl md:text-2xl">
+          {title}
+        </h3>
+
+        <div>
+          {authors?.map((author) => (
+            <span key={author} className="text-sm">
+              {author}
+            </span>
+          ))}
+        </div>
+
+        <p className="mt-4 text-sm md:text-base">{summary}</p>
+      </figcaption>
+    </figure>
+  );
+}
